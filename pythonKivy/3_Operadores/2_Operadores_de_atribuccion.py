19a7dc38fbfe8c394f9169b1252da84e9ae87075
JURIBA <40255771+RIBADISTRO@users.noreply.github.com>
2019-12-18 01:50:57 -0600

    Add files via upload

---
+++ pythonKivy/3_Operadores/2_Operadores_de_atribuccion.py
@@ -0,0 +1,33 @@
+"""
+OPERADORES
+Atribucion X Comparcion
+
+        =
+        ==
+    Nombre= Valor
+    /           \
+Nombre de la    Valor Atributo
+Variable
+=================
+||  Atribucion ||
+||             ||
+||    x = y    ||
+=================
+
+==================
+||              ||
+|| Comparacion  ||
+||   x == y     ||
+==================
+
+OPERAODRES DEATRIBUCION
+
+El valor al lado derecho del operador es atribuido
+a la varible al lado izquierdo del operdaor
+
+        x = y
+
+
+        i
+
+"""
