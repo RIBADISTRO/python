b819526d8186f068b31cc97578abf12df990be80
RIBADISTRO <ribali0.96@gmail.com>
2019-12-21 00:12:04 -0600

    Agregando funciones de python

---
+++ pythonKivy/5_Estructura_de_datos/2_Ejercicio.py
@@ -0,0 +1,12 @@
+a = 10
+b = 12
+c = 60
+
+x = int(input("Digite Un numero: "))
+
+#if(x == a or x == b or x ==c):
+#if(x in [10,12,60]):
+if (x in [a, b, c]):
+    print("El numero esta contenido")
+else:
+    print("NO ESTA CONTENIDO")
